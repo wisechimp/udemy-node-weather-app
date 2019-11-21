336c835aa65bda6f56ba68f832f88762b0891495
shicks1080 <stuart.hicks@gmail.com>
2019-11-21 15:04:59 +0100

    Exercise to extract a little data
    This exercise if simply to get a small amount of data from the request to give a simple forecast.

--- app.js
+++ app.js
@@ -3,5 +3,7 @@ const request = require('request')
 const url = 'https://api.darksky.net/forecast/301f7c4420ad29d44135ac28b021da44/37.8267,-122.4233'
 
 request({ url, json:true }, (error, response) => {
-    console.log(response.body.currently)
+    //console.log(response.body.currently)
+
+    console.log("It is currently " + response.body.currently.temperature + " degrees out. There is a " + response.body.currently.precipProbability + "% chance of rain.")
 })
