70fbf7afb93d9ff0c46519900aac1034b17404f7
shicks1080 <stuart.hicks@gmail.com>
2019-11-21 15:45:19 +0100

    Quick update

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+require('dotenv').config()
 const request = require('request')
 
-const url = 'https://api.darksky.net/forecast/301f7c4420ad29d44135ac28b021da44/37.8267,-122.4233'
+const url = 'https://api.darksky.net/forecast/' + process.env.DARKSKY + '/37.8267,-122.4233?units=si'
+console.log(url)
+
+const mapboxToken = 'pk.eyJ1Ijoid2lzZWNoaW1wIiwiYSI6ImNqcnRia21rOTBqaWQ0M25wM2d6N3o0NnMifQ.FTiAf09nKL12rrVaAFsZZQ'
 
 request({ url, json:true }, (error, response) => {
     //console.log(response.body.currently)
