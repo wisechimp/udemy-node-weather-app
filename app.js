7f1b278d71a99f8f209cce8b2774136c2d6ef9a4
shicks1080 <stuart.hicks@gmail.com>
2019-11-21 14:55:57 +0100

    Parsing the data automatically
    Parsing the request using the property json true rather than actively parsing the data received from the request afterwards.

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const request = require('request')
 
 const url = 'https://api.darksky.net/forecast/301f7c4420ad29d44135ac28b021da44/37.8267,-122.4233'
 
-request({ url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.currently)
+request({ url, json:true }, (error, response) => {
+    console.log(response.body.currently)
 })
