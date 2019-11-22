ba0eb35ac34eede1ca607d7dfea094a046969359
shicks1080 <stuart.hicks@gmail.com>
2019-11-22 12:56:53 +0100

    Creating a separate geocode function
    Writing the function in the app.js and them extracting it so it can be used wherever by sticking it in a utils folder and exporting. This function takes an address and returns a placename and coordinates.

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
 require('dotenv').config()
 const request = require('request')
+const geocode = require('./utils/geocode')
 
-const url = 'https://api.darksky.net/forecast/' + process.env.DARKSKY + '/37.8267,-122.4233?units=si'
-console.log(url)
+// const url = 'https://api.darksky.net/forecast/' + process.env.DARKSKY + '/37.8267,-122.4233?units=si'
+// console.log(url)
+//
+// const geocodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=' + process.env.MAPBOXTOKEN + '&limit=1'
+// console.log(geocodeUrl)
+//
+// request({ url, json:true }, (error, response) => {
+//   if (error) {
+//     console.log('Unable to connect to the weather service.')
+//   } else if (response.body.error) {
+//     console.log(response.body.error)
+//   } else {
+//     console.log("It is currently " + response.body.currently.temperature + " degrees out. There is a " + response.body.currently.precipProbability + "% chance of rain.")
+//   }
+// })
 
-const geocodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=' + process.env.MAPBOXTOKEN + '&limit=1'
-console.log(geocodeUrl)
-
-request({ url, json:true }, (error, response) => {
-  if (error) {
-    console.log('Unable to connect to the weather service.')
-  } else if (response.body.error) {
-    console.log(response.body.error)
-  } else {
-    console.log("It is currently " + response.body.currently.temperature + " degrees out. There is a " + response.body.currently.precipProbability + "% chance of rain.")
-  }
-})
-
-request({ url: geocodeUrl, json:true}, (error, response) => {
-  if (error) {
-    console.log('Unable to connect to the geocoding service.')
-  } else if (response.body.message) {
-    console.log(response.body.message)
-  } else {
-    console.log("Los Angeles is at: " + response.body.features[0].center[1] + ", " + response.body.features[0].center[0])
-  }
+geocode('Staffanstorp', (error, data) => {
+  console.log('Error', error)
+  console.log('Data', data)
 })
